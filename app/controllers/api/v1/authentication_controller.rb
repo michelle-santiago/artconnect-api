63e24d395aa1d97bb3016f8ad5ae76761e929be5
michelle-santiago <mdcsantiago0@gmail.com>
2023-05-13 11:59:52 +0800

    refactor: add namespace

---
+++ app/controllers/api/v1/authentication_controller.rb
@@ -0,0 +1,29 @@
+module Api
+    module V1
+        class AuthenticationController < ApplicationController
+            skip_before_action :authenticate_user, only: [:sign_in]
+            
+            def sign_in
+                if user_params[:email].empty? || user_params[:password].empty?
+                    render json: { error: "Fields can't be blank", status: 500 }
+                else
+                    @user = User.find_by_email(user_params[:email])
+                    if @user&.authenticate(user_params[:password])
+                        token = jwt_encode(user_id: @user.id)
+                        time = Time.now + 24.hours.to_i
+                        render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"),
+                                       username: @user.username }, status: 200
+                    else
+                        render json: { error: "Invalid email/password credentials", status: 401 }
+                    end
+                end
+            end
+        
+            private
+            
+            def user_params
+                params.permit(:email, :password)
+            end
+        end        
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   
-  post '/sign_in' => 'authentication#sign_in'
+  namespace :api do
+    namespace :v1 do
+      post '/sign_in' => 'authentication#sign_in'
   
-  post '/sign_up' => 'user#sign_up'
-  
-  get 'home' => 'home#index'
+      post '/sign_up' => 'user#sign_up'
+      
+      get 'home' => 'home#index'
+    end
+  end
+
+
 end
   
