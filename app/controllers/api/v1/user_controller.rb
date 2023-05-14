d3abf4ee85fff8ffc47942ca5429f911be661f33
michelle-santiago <mdcsantiago0@gmail.com>
2023-05-14 09:11:16 +0800

    refactor: indention

--- app/controllers/api/v1/user_controller.rb
+++ app/controllers/api/v1/user_controller.rb
@@ -1,29 +1,29 @@
 
 module Api
-    module V1
-        class UserController < ApplicationController
-            skip_before_action :authenticate_user, only: [:sign_up]
+	module V1
+		class UserController < ApplicationController
+			skip_before_action :authenticate_user, only: [:sign_up]
 
-            def sign_up
-                @user = User.new(user_params)
-                if @user.valid?
-                    if @user.password === user_params[:password_confirmation]
-                        if @user.save
-                            render json: @user, status: 201            
-                        else
-                            render json: { errors: @user.errors.full_messages}, status: 500
-                        end
-                    end
-                else
-                    render json: { errors: @user.errors.full_messages}, status: 500   
-                end
-            end
+			def sign_up
+				@user = User.new(user_params)
+				if @user.valid?
+					if @user.password === user_params[:password_confirmation]
+						if @user.save
+							render json: @user, status: 201            
+						else
+							render json: { errors: @user.errors.full_messages}, status: 500
+						end
+					end
+				else
+					render json: { errors: @user.errors.full_messages}, status: 500   
+				end
+			end
 
-            private
+			private
 
-            def user_params
-                params.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :role)
-            end
-        end
-    end
+			def user_params
+				params.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :role)
+			end
+		end
+	end
 end
