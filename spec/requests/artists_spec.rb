e90d441c8ae234f2a498287a3ce37b994f60350b
michelle-santiago <mdcsantiago0@gmail.com>
2023-05-17 17:35:59 +0800

    refacotor: remove print

--- spec/requests/artists_spec.rb
+++ spec/requests/artists_spec.rb
@@ -23,7 +23,6 @@ RSpec.describe "artists", type: :request do
   describe "GET /index as an artist" do
     it "renders a successful response" do
       get api_v1_artists_url, headers: { Authorization: jwt_encode(artist) }
-      print JSON.parse(response.body)
       expect(response).to have_http_status(200)
     end
   end
