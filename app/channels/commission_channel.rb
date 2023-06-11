1ef3d3df8d365b68d9af1035cec67e03f740d146
michelle-santiago <mdcsantiago0@gmail.com>
2023-06-11 13:55:14 +0800

    feat: unsubscribe from actioncable

--- app/channels/commission_channel.rb
+++ app/channels/commission_channel.rb
@@ -2,4 +2,8 @@ class CommissionChannel < ApplicationCable::Channel
   def subscribed
     stream_from "commission_channel#{params[:id]}"
   end
+
+  def unsubscribed
+    stop_stream_from "commission_channel#{params[:id]}"
+  end
 end
--- app/channels/direct_channel.rb
+++ app/channels/direct_channel.rb
@@ -2,4 +2,8 @@ class DirectChannel < ApplicationCable::Channel
   def subscribed
     stream_from "direct_channel#{params[:id]}"
   end
+
+  def unsubscribed
+    stop_stream_from "direct_channel#{params[:id]}"
+  end
 end
